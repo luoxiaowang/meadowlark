bd075543872471a007de422b379be834fcff89f8
luoxiaowang <15927510358@163.com>
2015-12-03 18:19:27 +0800

    新增handlebars视图模板处理

--- meadowlark.js
+++ meadowlark.js
@@ -1,34 +1,51 @@
 var express = require("express");
 var app = express();
 
+//设置handlebars视图引擎
+//用main来作为默认布局main.handlebars
+var handlebars = require("express3-handlebars").create({defaultLayout:'main'});
+app.engine("handlebars",handlebars.engine);
+app.set("view engine",'handlebars');
+
+
 //环境变量要是设置了PORT，那么就用环境变量的PORT
 //PORT=8080 node app.js
 //supervisor
 app.set("port",process.env.PORT || 3000);
 
+//静态资源目录，放在路由前面
+app.use(express.static(__dirname + '/public'));
+
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
+
 //添加路由
 app.get("/",function(req,res){
-    res.type("text/plain");
-    res.send("Meadowlark Travel");
+    res.render("home");
 });
 app.get("/about",function(req,res){
-    res.type("text/plain");
-    res.send("About Meadowlark Travel");
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render("about",{fortune:randomFortune});
 });
 
 //404    app.use->中间件  没有路由器匹配将会执行的处理器   路由和中间件的顺序很重要
 app.use(function(req,res){
-    res.type("text/plain");
     res.status("404");
-    res.send("404 - Not Found");
+    res.render("404");
 });
 
 //500   会根据参数个数区分404还是500
 app.use(function(err,req,res,next){
     console.error(err.stack);
-    res.type("text/plain");
     res.status("500");
-    res.send("500 - Server Error");
+    res.render("500");
 });
 
 app.listen(app.get("port"),function(){
